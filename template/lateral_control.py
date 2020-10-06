48c6d39a38d973a549eaa198ab2a183449311221
ShoumikMajumdar <shoumikmajumdar@gmail.com>
2020-10-05 22:00:55 -0400

    Lateral working code

--- template/lateral_control.py
+++ template/lateral_control.py
@@ -19,7 +19,8 @@ class LateralController:
     '''
 
 
-    def __init__(self, gain_constant=5, damping_constant=0.6):
+    def __init__(self, gain_constant=5, damping_constant=0.1):
+        #damping_constant = 0.6
 
         self.gain_constant = gain_constant
         self.damping_constant = damping_constant
@@ -34,9 +35,9 @@ class LateralController:
             waypoints (np.array) [2, num_waypoints]
             speed (float)
         '''
-        waypoints = np.array([[4.74299617e+01, 4.74296727e+01, 4.75480819e+01, 4.76715591e+01,
-                               4.78097113e+01, 4.80111628e+01],[2.11004204e-02, 1.33729666e+01 ,2.69786568e+01, 4.02988814e+01,
-                                     5.36131275e+01, 6.69793708e+01]])
+#        waypoints = np.array([[4.74299617e+01, 4.74296727e+01, 4.75480819e+01, 4.76715591e+01,
+#                               4.78097113e+01, 4.80111628e+01],[2.11004204e-02, 1.33729666e+01 ,2.69786568e+01, 4.02988814e+01,
+#                                     5.36131275e+01, 6.69793708e+01]])
 
         #print(waypoints)
         A = np.reshape(waypoints[:,0],(2,-1))
@@ -46,13 +47,13 @@ class LateralController:
         #print(_A.shape)
         D = np.dot(C.T,_A)
         D = D/np.linalg.norm(C)
+        print(f"D {D}")
         #print(D.shape)
         ori_error = np.arccos(D).item()
-        #print(ori_error)
+        print(f'ORIENTATION {ori_error}')
         car_pos = np.reshape(np.array([48,0]),(2,-1))
-        #cross_track = np.abs(A-car_pos)[0].item()
-        cross_track = 0
-        sc = ori_error + np.arctan((self.gain_constant*cross_track)/speed)
+        cross_track = (A[0]-car_pos[0]).item()
+        sc = ori_error + np.arctan((self.gain_constant*cross_track)/(speed+0.1))
         print(f'Cross track {cross_track}')
 
         
@@ -67,10 +68,12 @@ class LateralController:
         # derive damping term
         
         steering_angle = sc - self.damping_constant*(sc-self.previous_steering_angle)
-        print(f'SC {steering_angle}')
+        #print(f'SC {steering_angle}')
+        #steering_angle = sc
         self.previous_steering_angle = steering_angle
         
         # clip to the maximum stering angle (0.4) and rescale the steering action space
+        print(f'STEER {np.clip(steering_angle, -0.4, 0.4) / 0.4}')
         return np.clip(steering_angle, -0.4, 0.4) / 0.4
 
 
